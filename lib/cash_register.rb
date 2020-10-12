d51d1f9f14a339959f929f35b2f322f0fb9933ee
Firas Chaabani <firaschaabani1@gmail.com>
2020-10-12 00:08:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,8 +4,10 @@ attr_accessor :total, :discount, :items, :prices, :quantity
 
 
 def intialize (prices)
-  @
+  @price = price
+  price.new = price+1
 end
+
 def cash_register_with_discount
   CashRegister = new (20)
   self.CashRegister
